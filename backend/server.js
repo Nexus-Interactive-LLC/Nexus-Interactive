bf6c9ba4eaa03f6666781629fa02eba43cf0f5a8
aad3m <aliadem0205@gmail.com>
2024-12-07 00:11:26 -0600

    connected backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const partnerRoutes = require('./routes/partners');
 const serviceRoutes = require('./routes/services');
 const contactRoutes = require('./routes/contact');
 
 const app = express();
+app.use(cors()); // Enable CORS for cross-origin requests
 app.use(bodyParser.json()); // To parse JSON bodies
 
 // Connect to MongoDB
 require('./db');  // Import MongoDB connection setup
 
-// Use routes
-app.use('/partners', partnerRoutes);
-app.use('/services', serviceRoutes);
-app.use('/contact', contactRoutes);
-
-const cors = require('cors');
-app.use(cors());
+// Use routes with '/api' prefix
+app.use('/api/partners', partnerRoutes);
+app.use('/api/services', serviceRoutes);
+app.use('/api/contact', contactRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
